e11c1373006e5293dce615cf2232c3b239220fec
Robert Speicher <rspeicher@gmail.com>
2011-04-02 02:25:02 -0400

    Add GamerTag resource

---
+++ app/models/live_resources/gamer_tag.rb
@@ -0,0 +1,104 @@
+class GamerTag < LiveResource
+  # @see {achievements}
+  class Achievement < Struct.new(:site_id, :name, :secret, :value, :tile_path); end
+
+  # @see {games}
+  class Game < Struct.new(:name, :guid, :title_id); end
+
+  def initialize(tag)
+    @tag = tag
+  end
+
+  # Returns an array of this {GamerTag}'s games
+  #
+  # Each array entry is a {GamerTag::Game} instance, which is a
+  # <tt>Struct</tt> with the following attributes:
+  # * <tt>name</tt>
+  # * <tt>guid</tt>
+  # * <tt>title_id</tt>
+  #
+  # @return [Array]
+  def games
+    doc = Nokogiri::HTML(fetch_games)
+
+    games = []
+
+    doc.css('div.LineItem').each do |div|
+      game = Game.new
+
+      # Extract name from h3
+      game.name = div.css('div.TitleInfo h3').first.text
+
+      # Extract guid from game link
+      game.guid = div.css('div.TitleInfo a').first['href'].gsub(/.*\/([^\/]+)$/, '\1')
+
+      # Extract title_id from "Compare Achievements" link
+      game.title_id = div.css('div.lastgridchild a').first['href'].gsub(/.*titleId=(\d+).*/, '\1')
+
+      games << game
+    end
+
+    games
+  end
+
+  # Returns an array of <tt>GamerTag::Achievements</tt> for a specific game.
+  #
+  # Each array entry is a {GamerTag::Achievement} instance, which is a
+  # <tt>Struct</tt> with the following attributes:
+  # * <tt>site_id</tt>
+  # * <tt>name</tt>
+  # * <tt>secret</tt>
+  # * <tt>value</tt>
+  # * <tt>tile_path</tt>
+  #
+  # @param [String] title_id The game's title_id attribute
+  # @return [Array]
+  def achievements(title_id)
+    doc = Nokogiri::HTML(fetch_achievements(title_id))
+
+    achievements = []
+
+    doc.css('div.SpaceItem').each do |div|
+      ach = Achievement.new
+
+      # Extract site_id from div ID
+      ach.site_id = div.css('div.AchievementInfo').first['id'].gsub(/^ach-(\d+)$/, '\1')
+
+      # Extract name from h3
+      ach.name = div.css('div.AchievementInfo h3').first.text
+
+      # Determine secret from title
+      ach.secret = ach.name == "Secret Achievement"
+
+      # Extract value from GamerScore div
+      ach.value = div.css('div.GamerScore').first.text
+
+      # Extract tile_path from image
+      if ach.secret
+        ach.tile_path = ""
+      else
+        ach.tile_path = div.css('img.AchievementIcon').first['src'].gsub(/.*tiles\/(.+)\.(jpg|png)$/, '\1')
+      end
+
+      achievements << ach
+    end
+
+    achievements
+  end
+
+  private
+
+  # Fetches the response body for this tag's played games page
+  def fetch_games
+    response = self.class.get('/en-US/GameCenter', :query => {:compareTo => @tag})
+
+    response.body
+  end
+
+  # Fetches the response body for this tag's achievements for a specific game
+  def fetch_achievements(title_id)
+    response = self.class.get('/en-US/Achievements', :query => {:titleId => title_id, :compareTo => @tag})
+
+    response.body
+  end
+end
---
+++ spec/models/live_resources/gamer_tag_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe GamerTag, "#games" do
+  before do
+    FakeWeb.register_uri(:get, /GameCenter.+saigoh/, :body => fixture('xbox_live/games_saigoh.html'))
+    @resource = GamerTag.new('saigoh')
+    @games    = @resource.games
+  end
+
+  it "should return an Array of GamerTag::Game instances" do
+    @games.should be_kind_of(Array)
+    @games[0].should be_kind_of(GamerTag::Game)
+  end
+
+  describe GamerTag::Game do
+    subject { @games.detect { |g| g.name == "Super Meat Boy" } }
+
+    it             { should_not be_nil }
+    its(:name)     { should eql("Super Meat Boy") } # Redundant, but whatevs.
+    its(:guid)     { should eql("66acd000-77fe-1000-9115-d80258410a5a") }
+    its(:title_id) { should eql("1480657498") }
+  end
+end
+
+describe GamerTag, "#achievements" do
+  before do
+    title_id = 1480657498 # Super Meat Boy
+    FakeWeb.register_uri(:get, /Achievements.+titleId=#{title_id}/, :body => fixture("xbox_live/achievements_saigoh_#{title_id}.html"))
+    @resource     = GamerTag.new('saigoh')
+    @achievements = @resource.achievements(title_id)
+  end
+
+  it "should return an Array of GamerTag::Achievement instances" do
+    @achievements.should be_kind_of(Array)
+    @achievements[0].should be_kind_of(GamerTag::Achievement)
+  end
+
+  describe GamerTag::Achievement do
+    context "normal achievement" do
+      subject { @achievements.detect { |a| a.name == "The Real End" } }
+
+      it              { should_not be_nil }
+      its(:site_id)   { should eql("34") }
+      its(:name)      { should eql("The Real End") }
+      its(:secret)    { should eql(false) }
+      its(:value)     { should eql("20") }
+      its(:tile_path) { should eql("Ui/MH/0jc8P2NhbC82FQUXXFJRbjVBL2FjaC8wL0IAAAABUFBQ-SgjSQ==") }
+    end
+
+    context "secret achievement" do
+      subject { @achievements.detect { |a| a.site_id == "28" } }
+
+      it              { should_not be_nil }
+      its(:site_id)   { should eql("28") }
+      its(:name)      { should eql("Secret Achievement") }
+      its(:secret)    { should eql(true) }
+      its(:value)     { should eql("10") }
+      its(:tile_path) { should eql("") }
+    end
+  end
+end
+
